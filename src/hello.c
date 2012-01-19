b3080e32dd1a4f2e82382fdb9a954fca9d78f76c
James Mitchell <jenkyfunds@gmail.com>
2012-01-19 12:33:58 +0000

    intermediate commit. JDM

--- src/hello.c
+++ src/hello.c
@@ -1,44 +1,10 @@
 #include <stdio.h>
 
-int main()
+void main()
 {
-   int a;
-   scanf("%d",&a);
-   a = a * 2;
-   printf("The answer is %d",a);
-   return 0;
+  printf("size of a short is %d\n", sizeof(short));
+  printf("size of a int is %d\n", sizeof(int));
+  printf("size of a long is %d\n", sizeof(long));
 }
 
-#include        "system.h"              /* system dependent part           */
-
-#include        "gasman.h"              /* garbage collector               */
-#include        "objects.h"             /* objects                         */
-#include        "scanner.h"             /* scanner                         */
-
-#include        "gap.h"                 /* error handling, initialisation  */
-
-#include        "gvars.h"               /* global variables                */
-
-#include        "calls.h"               /* generic call mechanism          */
-#include        "opers.h"               /* generic operations              */
-
-#include        "ariths.h"              /* basic arithmetic                */
-
-#include        "bool.h"                /* booleans                        */
-
-#include        "integer.h"             /* integers                        */
-
-#define INCLUDE_DECLARATION_PART
-#include        "permutat.h"            /* permutations                    */
-#undef  INCLUDE_DECLARATION_PART
-
-#include        "records.h"             /* generic records                 */
-#include        "precord.h"             /* plain records                   */
-
-#include        "lists.h"               /* generic lists                   */
-#include        "plist.h"               /* plain lists                     */
-#include        "range.h"               /* ranges                          */
-#include        "string.h"              /* strings                         */
-
-#include        "saveload.h"            /* saving and loading              */
 
