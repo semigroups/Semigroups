48707e2e1c801a31fc3ff52fd151d0feb93f792e
James Mitchell <jenkyfunds@gmail.com>
2012-07-12 14:02:59 +0100

    adding slp.gd. JDM

---
+++ gap/slp.gd
@@ -0,0 +1,14 @@
+#############################################################################
+###
+##W  slp.gd
+##Y  Copyright (C) 2011-12                                James D. Mitchell
+###
+###  Licensing information can be found in the README file of this package.
+###
+##############################################################################
+###
+
+DeclareGlobalFunction("LambdaOrbSLP");
+DeclareOperation("SemigroupEltSLP", [IsActingSemigroup, IsActingElt]);
+DeclareGlobalFunction("TraceRClassRepsTree");
+
