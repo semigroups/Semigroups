5fb869e3cda708f70314056f40430a10340ddf81
James Mitchell <jenkyfunds@gmail.com>
2013-09-12 09:17:49 +0100

    updating attributes.gd to 2.0. JDM

--- gap/attributes.gd
+++ gap/attributes.gd
@@ -8,28 +8,27 @@
 #############################################################################
 ##
 
-DeclareGlobalFunction("SemigroupActionHomomorphism");
-
-DeclareOperation("EmbeddingNC", [IsSemigroup, IsGreensHClass]);
-DeclareAttribute("GroupOfUnits", IsSemigroup);
-DeclareAttribute("IdempotentGeneratedSubsemigroup", IsSemigroup);
-DeclareAttribute("InjectionPrincipalFactor", IsGreensDClass);
-
+DeclareOperation("IsMaximalSubsemigroup", [IsSemigroup, IsSemigroup]);
+DeclareOperation("IrredundantGeneratingSubset",
+[IsAssociativeElementCollection]);
 DeclareOperation("InversesOfSemigroupElementNC", [IsActingSemigroup and
 HasGeneratorsOfSemigroup, IsAssociativeElement]);
 
-DeclareOperation("IsomorphismBipartitionSemigroup", [IsSemigroup]);
+DeclareAttribute("GroupOfUnits", IsSemigroup);
+DeclareAttribute("IdempotentGeneratedSubsemigroup", IsSemigroup);
+DeclareAttribute("InjectionPrincipalFactor", IsGreensDClass);
 DeclareAttribute("IsomorphismReesMatrixSemigroup", IsGreensDClass);
 DeclareAttribute("IsomorphismPermGroup", IsTransformationSemigroup);
-
 DeclareAttribute("MinimalIdeal", IsSemigroup);
-DeclareOperation("NrElementsOfRank", [IsSemigroup and
-HasGeneratorsOfSemigroup, IsPosInt]);
 DeclareAttribute("PosetOfIdempotents", IsSemigroup);
 DeclareAttribute("PrimitiveIdempotents", IsInverseSemigroup);
 DeclareAttribute("PrincipalFactor", IsGreensDClass);
-
 DeclareAttribute("MultiplicativeZero", IsActingSemigroup);
 DeclareAttribute("SmallGeneratingSet", IsActingSemigroup);
 DeclareAttribute("StructureDescription", IsBrandtSemigroup);
 DeclareAttribute("StructureDescription", IsGroupAsSemigroup);
+DeclareAttribute("StructureDescriptionSchutzenbergerGroups", IsActingSemigroup);
+DeclareAttribute("StructureDescriptionMaximalSubgroups", IsActingSemigroup);
+DeclareAttribute("MaximalDClasses", IsSemigroup);
+DeclareAttribute("MaximalSubsemigroups", IsSemigroup);
+DeclareAttribute("MinimalDClass", IsSemigroup);
