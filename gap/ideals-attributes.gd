bb5b0766b18930ca37dead205d4d63d3fe6892d2
Julius <unknown>
2014-03-21 15:44:59 +0000

    forgot to add ideals-attributes.gd. jj

---
+++ gap/ideals-attributes.gd
@@ -0,0 +1,12 @@
+############################################################################# 
+## 
+#W  ideals-attributes.gd
+#Y  Copyright (C) 2013-14                                 James D. Mitchell
+## 
+##  Licensing information can be found in the README file of this package. 
+## 
+############################################################################# 
+##
+
+DeclareOperation("SmallIdealGeneratingSet",
+[IsActingSemigroup and IsSemigroupIdeal]);
