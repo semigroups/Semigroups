07a13303c61bffb48fb7023caace9ea07c45bf48
mtorpey <unknown>
2014-06-13 16:43:01 +0100

    Altered .gd file

--- gap/inverse-cong.gd
+++ gap/inverse-cong.gd
@@ -1,2 +1,10 @@
-DeclareGlobalFunction("Trace");
+# Inverse Congruences By Congruence Pair
+DeclareCategory("IsInverseSemigroupCongruence",
+        IsSemigroupCongruence and IsAttributeStoringRep);
+DeclareAttribute("CongruencesOfSemigroup",
+        IsInverseSemigroup and IsFinite);
+DeclareGlobalFunction("InverseSemigroupCongruenceByCongruencePair");
+DeclareGlobalFunction("InverseSemigroupCongruenceByCongruencePairNC");
+
+DeclareAttribute("Trace", IsSemigroupCongruence);
 DeclareGlobalFunction("CongruencePair");
