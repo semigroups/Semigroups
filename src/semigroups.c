5bff0d72a4ef4449ae712a2d65fe491d8d603d61
James Mitchell <jdm3@st-and.ac.uk>
2014-09-10 16:49:40 +0100

    adding GABOW_SCC from semigroupe branch

--- src/semigroups.c
+++ src/semigroups.c
@@ -21,6 +21,143 @@
 
 //#include "pkgconfig.h"             /* our own configure results */
 
+/****************************************************************************
+**
+*F  FuncGABOW_SCC
+**
+** `digraph' should be a list whose entries and the lists of out-neighbours
+** of the vertices. So [[2,3],[1],[2]] represents the graph whose edges are
+** 1->2, 1->3, 2->1 and 3->2.
+**
+** returns a newly constructed record with two components 'comps' and 'id' the
+** elements of 'comps' are lists representing the strongly connected components
+** of the directed graph, and in the component 'id' the following holds:
+** id[i]=PositionProperty(comps, x-> i in x);
+** i.e. 'id[i]' is the index in 'comps' of the component containing 'i'.  
+** Neither the components, nor their elements are in any particular order.
+**
+** The algorithm is that of Gabow, based on the implementation in Sedgwick:
+**   http://algs4.cs.princeton.edu/42directed/GabowSCC.java.html
+** (made non-recursive to avoid problems with stack limits) and 
+** the implementation of STRONGLY_CONNECTED_COMPONENTS_DIGRAPH in listfunc.c.
+*/
+
+static Obj FuncGABOW_SCC(Obj self, Obj digraph)
+{
+  UInt len1, len2, count, level, w, v, n, l, idw, *fptr, *stkptr;
+  Obj  id, frames, adj, stack1, stack2, out, comp, comps; 
+ 
+  n = LEN_LIST(digraph);
+  if (n == 0){
+    out = NEW_PREC(2);
+    AssPRec(out, RNamName("id"), NEW_PLIST(T_PLIST_EMPTY+IMMUTABLE,0));
+    AssPRec(out, RNamName("comps"), NEW_PLIST(T_PLIST_EMPTY+IMMUTABLE,0));
+    CHANGED_BAG(out);
+    return out;
+  }
+
+  len1 = 0; 
+  stack1 = NEW_PLIST(T_PLIST_CYC, n); 
+  //stack1 is a plist so we can use memcopy below
+  SET_LEN_PLIST(stack1, n);
+
+  len2 = 0;
+  stack2 = NewBag(T_DATOBJ, (n+1)*sizeof(UInt));
+   
+  id = NEW_PLIST(T_PLIST_CYC+IMMUTABLE, n);
+  SET_LEN_PLIST(id, n);
+  //init id
+  for(v=1;v<=n;v++){
+    SET_ELM_PLIST(id, v, INTOBJ_INT(0));
+  }
+  count = n;
+  
+  comps = NEW_PLIST(T_PLIST_TAB+IMMUTABLE, n);
+  SET_LEN_PLIST(comps, 0);
+  
+  frames = NewBag(T_DATOBJ, (3*n+1)*sizeof(UInt));
+  
+  for(v=1;v<=n;v++){
+    if(INT_INTOBJ(ELM_PLIST(id, v))==0){
+      level=1;
+      adj = ELM_LIST(digraph, v);
+      PLAIN_LIST(adj);
+      fptr = (UInt *)ADDR_OBJ(frames);
+      fptr[0] = v; // vertex
+      fptr[1] = 1; // index
+      fptr[2] = (UInt)adj;
+      SET_ELM_PLIST(stack1, ++len1, INTOBJ_INT(v));
+      ((UInt *)ADDR_OBJ(stack2))[++len2] = len1;
+      SET_ELM_PLIST(id, v, INTOBJ_INT(len1)); 
+      
+      while(level>0){
+        if(fptr[1]>LEN_PLIST(fptr[2])){
+          if(((UInt *)ADDR_OBJ(stack2))[len2]==INT_INTOBJ(ELM_PLIST(id, fptr[0]))){
+            len2--;
+            count++;
+            l=0;
+            do{
+              l++;
+              w=INT_INTOBJ(ELM_PLIST(stack1, len1--));
+              SET_ELM_PLIST(id, w, INTOBJ_INT(count));
+            }while(w!=fptr[0]);
+            
+            comp = NEW_PLIST(T_PLIST_CYC+IMMUTABLE, l);
+            SET_LEN_PLIST(comp, l);
+           
+            memcpy( (void *)((char *)(ADDR_OBJ(comp)) + sizeof(Obj)), 
+                    (void *)((char *)(ADDR_OBJ(stack1)) + (len1+1)*sizeof(Obj)), 
+                    (size_t)(l*sizeof(Obj)));
+
+            l=LEN_PLIST(comps)+1;
+            SET_ELM_PLIST(comps, l, comp);
+            SET_LEN_PLIST(comps, l);
+            CHANGED_BAG(comps);
+            fptr = (UInt *)ADDR_OBJ(frames)+(level-1)*3;
+          }
+          level--;
+          fptr -= 3;
+        } else {
+          
+          w = INT_INTOBJ(ELM_PLIST(fptr[2], fptr[1]++));
+          idw = INT_INTOBJ(ELM_PLIST(id, w));
+          
+          if(idw==0){
+      
+            level++;
+            adj = ELM_LIST(digraph, w);
+            PLAIN_LIST(adj);
+            fptr += 3; 
+            fptr[0] = w; // vertex
+            fptr[1] = 1; // index
+            fptr[2] = (UInt)adj;                          
+            SET_ELM_PLIST(stack1, ++len1, INTOBJ_INT(w));
+            ((UInt *)ADDR_OBJ(stack2))[++len2] = len1;
+            SET_ELM_PLIST(id, w, INTOBJ_INT(len1)); 
+          
+          } else {
+            stkptr=((UInt*)ADDR_OBJ(stack2));
+            while(stkptr[len2]>idw){
+              len2--; // pop from stack2
+            }
+          }
+        }
+      }
+    }
+  }
+
+  for(v=1;v<=n;v++){
+    SET_ELM_PLIST(id, v, INTOBJ_INT(INT_INTOBJ(ELM_PLIST(id, v))-n));
+  }
+
+  out = NEW_PREC(2);
+  SHRINK_PLIST(comps, LEN_PLIST(comps));
+  AssPRec(out, RNamName("id"), id);
+  AssPRec(out, RNamName("comps"), comps);
+  CHANGED_BAG(out);
+  return out;
+}
+
 /*F * * * * * * * * * * * * * initialize package * * * * * * * * * * * * * * */
 
 /******************************************************************************
