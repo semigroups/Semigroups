761ae94f04b27b3dbd3433f8e7cadddab1975965
James Mitchell <jdm3@st-and.ac.uk>
2014-09-10 16:47:56 +0100

    copying kernel module from semigroupe branch.

---
+++ src/semigroups.c
@@ -0,0 +1,123 @@
+/***************************************************************************
+**
+*A  semigroups.c               Semigroups package             J. D. Mitchell 
+**
+**  Copyright (C) 2014 - J. D. Mitchell 
+**  This file is free software, see license information at the end.
+**  
+*/
+
+#include <stdlib.h>
+
+#include "src/compiled.h"          /* GAP headers                */
+
+#undef PACKAGE
+#undef PACKAGE_BUGREPORT
+#undef PACKAGE_NAME
+#undef PACKAGE_STRING
+#undef PACKAGE_TARNAME
+#undef PACKAGE_URL
+#undef PACKAGE_VERSION
+
+//#include "pkgconfig.h"             /* our own configure results */
+
+/*F * * * * * * * * * * * * * initialize package * * * * * * * * * * * * * * */
+
+/******************************************************************************
+*V  GVarFuncs . . . . . . . . . . . . . . . . . . list of functions to export
+*/
+static StructGVarFunc GVarFuncs [] = {
+
+  { 0 }
+
+};
+
+/******************************************************************************
+*F  InitKernel( <module> )  . . . . . . . . initialise kernel data structures
+*/
+static Int InitKernel ( StructInitInfo *module )
+{
+    /* init filters and functions                                          */
+    InitHdlrFuncsFromTable( GVarFuncs );
+
+    /* return success                                                      */
+    return 0;
+}
+
+/******************************************************************************
+*F  InitLibrary( <module> ) . . . . . . .  initialise library data structures
+*/
+static Int InitLibrary ( StructInitInfo *module )
+{
+    Int             i, gvar;
+    Obj             tmp;
+
+    /* init filters and functions */
+    for ( i = 0;  GVarFuncs[i].name != 0;  i++ ) {
+      gvar = GVarName(GVarFuncs[i].name);
+      AssGVar(gvar,NewFunctionC( GVarFuncs[i].name, GVarFuncs[i].nargs,
+                                 GVarFuncs[i].args, GVarFuncs[i].handler ) );
+      MakeReadOnlyGVar(gvar);
+    }
+
+    tmp = NEW_PREC(0);
+    gvar = GVarName("SEMIGROUPSC"); 
+    AssGVar( gvar, tmp ); 
+    MakeReadOnlyGVar(gvar);
+
+    /* return success                                                      */
+    return 0;
+}
+
+/******************************************************************************
+*F  InitInfopl()  . . . . . . . . . . . . . . . . . table of init functions
+*/
+static StructInitInfo module = {
+#ifdef SEMIGROUPSSTATIC
+ /* type        = */ MODULE_STATIC,
+#else
+ /* type        = */ MODULE_DYNAMIC,
+#endif
+ /* name        = */ "semigroups",
+ /* revision_c  = */ 0,
+ /* revision_h  = */ 0,
+ /* version     = */ 0,
+ /* crc         = */ 0,
+ /* initKernel  = */ InitKernel,
+ /* initLibrary = */ InitLibrary,
+ /* checkInit   = */ 0,
+ /* preSave     = */ 0,
+ /* postSave    = */ 0,
+ /* postRestore = */ 0
+};
+
+#ifndef SEMIGROUPSSTATIC
+StructInitInfo * Init__Dynamic ( void )
+{
+  return &module;
+}
+#endif
+
+StructInitInfo * Init__semigroups ( void )
+{
+  return &module;
+}
+
+/*
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; version 2 of the License.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
+
