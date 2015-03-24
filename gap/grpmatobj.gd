f0d9ee7a45bca230795799b482a8713d3ba29e82
Markus Pfeiffer <markus.pfeiffer@st-andrews.ac.uk>
2015-03-24 11:55:27 +0000

    Add files for MatrixObj groups

---
+++ gap/grpmatobj.gd
@@ -0,0 +1,12 @@
+############################################################################
+##
+#W  grpmatobj.gd
+#Y  Copyright (C) 2013-15                                James D. Mitchell
+##                                                       Markus Pfeiffer
+##
+##  Licensing information can be found in the README file of this package.
+##
+#############################################################################
+##
+
+DeclareCategory( "SEMIGROUPS_IsMatrix
---
+++ gap/grpmatobj.gi
@@ -0,0 +1,11 @@
+############################################################################
+##
+#W  grpmatobj.gd
+#Y  Copyright (C) 2013-15                                James D. Mitchell
+##                                                       Markus Pfeiffer
+##
+##  Licensing information can be found in the README file of this package.
+##
+#############################################################################
+##
+
