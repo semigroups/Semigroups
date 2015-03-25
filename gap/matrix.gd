cf6e31e3d46c3138e38d55f6022e93264779a030
James Mitchell <jdm3@st-and.ac.uk>
2015-03-25 10:02:31 +0000

    matrices

--- gap/matrix.gd
+++ gap/matrix.gd
@@ -1,7 +1,7 @@
 ############################################################################
 ##
 #W  matrix.gd
-#Y  Copyright (C) 2014                                   James D. Mitchell
+#Y  Copyright (C) 2015                                   James D. Mitchell
 ##                                                         Markus Pfeiffer
 ##
 ##  Licensing information can be found in the README file of this package.
