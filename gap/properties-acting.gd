68f49d1726b5545d81dbf7c51a1d2642df5469ff
James Mitchell <jdm3@st-and.ac.uk>
2015-04-23 11:22:26 +0100

    rearranging

--- gap/properties-acting.gd
+++
@@ -1,9 +0,0 @@
-#############################################################################
-##
-#W  properties-acting.gd
-#Y  Copyright (C) 2013-15                                James D. Mitchell
-##
-##  Licensing information can be found in the README file of this package.
-##
-#############################################################################
-##
