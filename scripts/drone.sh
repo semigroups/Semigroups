133a74d18733bb39051f276808f15d2003b89806
James Mitchell <jdm3@st-and.ac.uk>
2015-10-06 12:02:44 +0100

    update drone script.

--- scripts/drone.sh
+++ scripts/drone.sh
@@ -1,1 +1,1 @@
-echo "LoadPackage(\"semigroups\"); SemigroupsTestStandard(); quit;" | sh bin/gap.sh | tee testlog.txt | grep --colour=always -A 2 -E "########> Diff" ; ( ! grep "########> Diff" testlog.txt )
+echo "LoadPackage(\"semigroups\"); SemigroupsTestStandard(); quit;" | sh bin/gap.sh | tee testlog.txt | grep --colour=always -A 1 -E "########> Diff|$" ; ( ! grep "########> Diff" testlog.txt )
