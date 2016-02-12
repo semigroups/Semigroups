11f3759c13bc3256c67407a20c7a64afa8ac5c78
Michael Torpey <mct25@st-andrews.ac.uk>
2016-02-12 12:37:11 +0000

    drone.sh tries to check for more errors

--- scripts/drone.sh
+++ scripts/drone.sh
@@ -1,1 +1,1 @@
-echo "LoadPackage(\"semigroups\"); SemigroupsTestStandard(); SEMIGROUPS.TestManualExamples(); quit;" | sh bin/gap.sh | tee testlog.txt | grep --colour=always -A 1 -E "########> Diff|$" ; ( ! grep -E "########> Diff|# WARNING" testlog.txt )
+echo "LoadPackage(\"semigroups\"); SemigroupsTestStandard(); SEMIGROUPS.TestManualExamples(); quit; quit; quit;" | sh bin/gap.sh | tee testlog.txt | grep --colour=always -A 1 -E "########> Diff|$" ; ( ! grep -E "########> Diff|brk>|#E|Error|# WARNING" testlog.txt )
