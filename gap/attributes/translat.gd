b5ff05e75a4534f71fcfdd0b86253b83dc7267b8
Finlay Smith <fls3@st-andrews.ac.uk>
2017-02-22 22:53:45 +0000

    Update copyright notices

--- gap/attributes/translat.gd
+++ gap/attributes/translat.gd
@@ -1,7 +1,7 @@
 ############################################################################
 ##
 #W  translat.gd
-#Y  Copyright (C) 2015                                  James D. Mitchell
+#Y  Copyright (C) 2015-17                      James D. Mitchell, Finn Smith
 ##
 ##  Licensing information can be found in the README file of this package.
 ##
