c74d07f3155924e3a4d7a25daaa5d4c9111bbd1d
Wilf Wilson <wilf@wilf-wilson.net>
2017-03-24 16:33:22 +0000

    Update Travis build setup

--- scripts/travis-build.sh
+++ scripts/travis-build.sh
@@ -4,61 +4,96 @@ set -e
 # Display version of compiler
 $CXX --version
 
-# Store this directory
-SEMIDIR=$(pwd)
-
-# Get libsemigroups if appropriate
-if [ -d src ]
-then
-    cd src
-    git clone -b $LIBSEMIGROUPS_BR --depth=1 https://github.com/james-d-mitchell/libsemigroups.git
-    cd ..
-fi
-cd ..
+INITIALDIR=`pwd`
 
 # Download and compile GAP
-git clone -b $GAP_BRANCH --depth=1 https://github.com/$GAP_FORK/gap.git
+cd ..
+git clone -b $GAP_BRANCH --depth=1 https://github.com/$GAP_FORK/gap.git gap
 cd gap
 ./configure --with-gmp=system $GAP_FLAGS
 make
 mkdir pkg
 cd ..
 
-# Compile the Semigroups package
-mv $SEMIDIR gap/pkg/semigroups
-cd gap/pkg/semigroups
-if [ -d src ]
-then
-    ./autogen.sh
-    ./configure $PKG_FLAGS
-    make
-fi
-cd ../..
+# Move the package itself to the correct location
+mv $INITIALDIR gap/pkg/$PACKAGE
+cd gap/pkg/$PACKAGE
 
-# Get the packages
-cd pkg
+if [ "$PACKAGE" == "semigroups" ] && [ -d src ]; then
+  cd src
+  git clone -b $LIBSEMIGROUPS_BR --depth=1 https://github.com/james-d-mitchell/libsemigroups.git libsemigroups
+  cd ..
+  ./autogen.sh
+  ./configure $PKG_FLAGS
+  make
+fi
+
+if [ "$PACKAGE" == "digraphs" ]; then
+  curl -L -O https://gap-packages.github.io/Digraphs/$DIGRAPHS_LIB.tar.gz
+  tar xf $DIGRAPHS_LIB.tar.gz
+  rm $DIGRAPHS_LIB.tar.gz
+fi
+
+cd ..
+
+# Download and install dependencies and tools...
+
+# Compile Digraphs
+if [ "$PACKAGE" == "semigroups" ]; then
+  git clone -b $DIGRAPHS_BR --depth=1 https://github.com/gap-packages/Digraphs.git digraphs
+fi
+cd digraphs
+./autogen.sh
+./configure $PKG_FLAGS
+make
+cd ..
+
+# GAPDoc
 echo "Downloading $GAPDOC..."
-curl -LO http://www.gap-system.org/pub/gap/gap4/tar.gz/packages/$GAPDOC.tar.gz
+curl -O https://www.gap-system.org/pub/gap/gap4/tar.gz/packages/$GAPDOC.tar.gz
 tar xzf $GAPDOC.tar.gz
 rm $GAPDOC.tar.gz
+
+# GenSS
+echo "Downloading $GENSS..."
+curl -O https://www.gap-system.org/pub/gap/gap4/tar.gz/packages/$GENSS.tar.gz
+tar xzf $GENSS.tar.gz
+rm $GENSS.tar.gz
+
+# IO
 echo "Downloading $IO..."
-curl -LO http://www.gap-system.org/pub/gap/gap4/tar.gz/packages/$IO.tar.gz
+curl -O https://www.gap-system.org/pub/gap/gap4/tar.gz/packages/$IO.tar.gz
 tar xzf $IO.tar.gz
 rm $IO.tar.gz
 cd $IO
 ./configure $PKG_FLAGS
 make
 cd ..
+
+# Orb
 echo "Downloading $ORB..."
-curl -LO http://www.gap-system.org/pub/gap/gap4/tar.gz/packages/$ORB.tar.gz
+curl -O https://www.gap-system.org/pub/gap/gap4/tar.gz/packages/$ORB.tar.gz
 tar xzf $ORB.tar.gz
 rm $ORB.tar.gz
 cd $ORB
 ./configure $PKG_FLAGS
 make
 cd ..
+
+# Grape: only install if we're testing the digraphs package
+if [ "$PACKAGE" == "digraphs" ]; then
+  echo "Downloading $GRAPE..."
+  curl -O https://www.gap-system.org/pub/gap/gap4/tar.gz/packages/$GRAPE.tar.gz
+  tar xzf $GRAPE.tar.gz
+  rm $GRAPE.tar.gz
+  cd grape
+  ./configure $PKG_FLAGS
+  make
+  cd ..
+fi
+
 # Only install profiling package if we'll run coverage checks
-if [ ! -z "$COVERAGE" ]; then
+if [ ! -z "$RUNCOVERAGE" ]; then
   echo "Downloading $PROFILING..."
   curl -LO https://github.com/gap-packages/profiling/releases/download/v$PROFILING/profiling-$PROFILING.tar.gz
   tar xzf profiling-$PROFILING.tar.gz
@@ -70,17 +105,10 @@ if [ ! -z "$COVERAGE" ]; then
   make
   cd ..
 fi
-echo "Downloading $GENSS..."
-curl -LO http://www.gap-system.org/pub/gap/gap4/tar.gz/packages/$GENSS.tar.gz
-tar xzf $GENSS.tar.gz
-rm $GENSS.tar.gz
-git clone -b $DIGRAPHS_BR --depth=1 https://github.com/gap-packages/Digraphs.git digraphs
-cd digraphs
-./autogen.sh
-./configure $PKG_FLAGS
-make
-cd ../../..
 
-# Get gaplint
-echo "Downloading gaplint..."
-git clone -b master --depth=1 https://github.com/james-d-mitchell/gaplint
+# Only install gaplint if we'll run the linting checks
+if [ ! -z "$RUNGAPLINT" ]; then
+  echo "Downloading gaplint..."
+  git clone -b master --depth=1 https://github.com/james-d-mitchell/gaplint gaplint
+  cd ..
+fi
--- scripts/travis-test.sh
+++ scripts/travis-test.sh
@@ -2,31 +2,46 @@
 set -e
 set -o pipefail
 
-# Run the standard tests and manual examples
+# Start in the GAP directory
 cd ../..
-echo -e "\nRunning standard tests and manual examples..."
-echo "LoadPackage(\"semigroups\"); SemigroupsTestStandard(); SEMIGROUPS.TestManualExamples(); quit; quit; quit;" | bin/gap.sh -A -r -m 1g -T 2>&1 | tee testlog.txt
+GAPDIR=`pwd`
 
-# Run the SaveWorkspace tests
-echo -e "\nRunning SaveWorkspace tests..."
-echo "LoadPackage(\"semigroups\", false); SemigroupsTestInstall(); Test(\"pkg/semigroups/tst/workspaces/save-workspace.tst\"); quit; quit; quit;" | bin/gap.sh -A -r -m 1g -T 2>&1 | tee -a testlog.txt
-echo "LoadPackage(\"semigroups\", false); Test(\"pkg/semigroups/tst/workspaces/load-workspace.tst\"); SemigroupsTestInstall(); quit; quit; quit;" | bin/gap.sh -L pkg/semigroups/tst/workspaces/test-output.w -A -r -m 1g -T 2>&1 | tee -a testlog.txt
-rm pkg/semigroups/tst/workspaces/test-output.w
+# Create the testlog and remember its location
+touch testlog.txt
+TESTLOG="$GAPDIR/testlog.txt"
+
+cd pkg/$PACKAGE
 
 # Run gaplint
-cd pkg/semigroups
-echo -e "\nRunning gaplint..."
-../../../gaplint/gaplint.py gap/*/*.gi | tee -a ../../testlog.txt
+if [ ! -z "$RUNGAPLINT" ]; then
+  echo -e "\nRunning gaplint..."
+  export PATH=$PATH:$GAPDIR/pkg/gaplint
+  make lint
+else
+  echo -e "\nNot running gaplint..."
+fi
+
+# Run the SaveWorkspace tests
+cd tst/workspaces
+echo -e "\nRunning SaveWorkspace tests..."
+echo "LoadPackage(\"$PACKAGE\", false); SemigroupsTestInstall(); Test(\"save-workspace.tst\"); quit; quit; quit;" | $GAPDIR/bin/gap.sh -A -r -m 1g -T 2>&1 | tee -a $TESTLOG
+echo "Test(\"load-workspace.tst\"); SemigroupsTestInstall(); quit; quit; quit;" | $GAPDIR/bin/gap.sh -L test-output.w -A -r -m 1g -T 2>&1 | tee -a $TESTLOG
+rm test-output.w
+cd ../..
+
+# Run all tests and manual examples
+echo -e "\nRunning tests and manual examples..."
+echo "LoadPackage(\"$PACKAGE\"); SemigroupsTestStandard(); SEMIGROUPS.TestManualExamples(); quit; quit; quit;" | $GAPDIR/bin/gap.sh -A -r -m 1g -T 2>&1 | tee $TESTLOG
 
 # Run coverage checks
-if [ ! -z "$COVERAGE" ]; then
+if [ ! -z "$RUNCOVERAGE" ]; then
   echo -e "\nPerforming code coverage tests..."
-  for testfile in tst/standard/*.tst; do
-    filename=${testfile##*/}
-    if [ ! `grep -E "$filename" .covignore` ]; then
-      ../digraphs/scripts/travis-coverage.py $testfile 95 | tee -a ../../testlog.txt
+  for TESTFILE in tst/standard/*.tst; do
+    FILENAME=${TESTFILE##*/}
+    if [ ! `grep -E "$FILENAME" .covignore` ]; then
+      ../digraphs/scripts/travis-coverage.py $TESTFILE $THRESHOLD | tee -a $TESTLOG
     else
-      echo -e "\033[35mignoring $filename, since it is listed in .covignore\033[0m"
+      echo -e "\033[35mignoring $FILENAME, since it is listed in .covignore\033[0m"
     fi
   done
 else
@@ -34,4 +49,4 @@ else
 fi
 
 # Check the logs for invalid phrases
-( ! grep -E "Diff|brk>|#E|Error|error|# WARNING|fail|Syntax warning|Couldn't open saved workspace|insufficient|WARNING|FAILED" ../../testlog.txt )
+( ! grep -E "Diff|brk>|#E|Error|error|# WARNING|fail|Syntax warning|Couldn't open saved workspace|insufficient|WARNING in|FAILED|Total errors found:" $TESTLOG )
