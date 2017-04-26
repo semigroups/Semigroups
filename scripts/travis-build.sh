6cea6e1cee3d242994a06e49da9fb97b7e2acb7b
Wilf Wilson <wilf@wilf-wilson.net>
2017-04-27 00:39:19 +0100

    travis: separate the suites for robustness

--- scripts/travis-build.sh
+++ scripts/travis-build.sh
@@ -4,112 +4,85 @@ set -e
 # Display version of compiler
 $CXX --version
 
-INITIALDIR=`pwd`
-
-# Download and compile GAP
-cd ..
-git clone -b $GAP_BRANCH --depth=1 https://github.com/$GAP_FORK/gap.git gap
-cd gap
-./autogen.sh
-./configure --with-gmp=system $GAP_FLAGS
-make
-mkdir pkg
 cd ..
 
-# Move the package itself to the correct location
-mv $INITIALDIR gap/pkg/$PACKAGE
-cd gap/pkg/$PACKAGE
+# Assume: linting and running GAP are mutually-exclusive test suites
 
-if [ "$PACKAGE" == "semigroups" ] && [ -d src ]; then
-  cd src
-  git clone -b $LIBSEMIGROUPS_BR --depth=1 https://github.com/james-d-mitchell/libsemigroups.git libsemigroups
-  cd ..
-  ./autogen.sh
-  ./configure $PKG_FLAGS
-  make
-fi
+if [ ! -z "$LINT" ]; then
 
-if [ "$PACKAGE" == "digraphs" ]; then
-  curl -L -O https://gap-packages.github.io/Digraphs/$DIGRAPHS_LIB.tar.gz
-  tar xf $DIGRAPHS_LIB.tar.gz
-  rm $DIGRAPHS_LIB.tar.gz
-fi
-
-cd ..
-
-# Download and install dependencies and tools...
-
-# Compile Digraphs
-if [ "$PACKAGE" == "semigroups" ]; then
-  git clone -b $DIGRAPHS_BR --depth=1 https://github.com/gap-packages/Digraphs.git digraphs
-fi
-cd digraphs
-./autogen.sh
-./configure $PKG_FLAGS
-make
-cd ..
-
-# GAPDoc
-echo "Downloading $GAPDOC..."
-curl -O https://www.gap-system.org/pub/gap/gap4/tar.gz/packages/$GAPDOC.tar.gz
-tar xzf $GAPDOC.tar.gz
-rm $GAPDOC.tar.gz
-
-# GenSS
-echo "Downloading $GENSS..."
-curl -O https://www.gap-system.org/pub/gap/gap4/tar.gz/packages/$GENSS.tar.gz
-tar xzf $GENSS.tar.gz
-rm $GENSS.tar.gz
-
-# IO
-echo "Downloading $IO..."
-curl -O https://www.gap-system.org/pub/gap/gap4/tar.gz/packages/$IO.tar.gz
-tar xzf $IO.tar.gz
-rm $IO.tar.gz
-cd $IO
-./configure $PKG_FLAGS
-make
-cd ..
-
-# Orb
-echo "Downloading $ORB..."
-curl -O https://www.gap-system.org/pub/gap/gap4/tar.gz/packages/$ORB.tar.gz
-tar xzf $ORB.tar.gz
-rm $ORB.tar.gz
-cd $ORB
-./configure $PKG_FLAGS
-make
-cd ..
-
-# Grape: only install if we're testing the digraphs package
-if [ "$PACKAGE" == "digraphs" ]; then
-  echo "Downloading $GRAPE..."
-  curl -O https://www.gap-system.org/pub/gap/gap4/tar.gz/packages/$GRAPE.tar.gz
-  tar xzf $GRAPE.tar.gz
-  rm $GRAPE.tar.gz
-  cd grape
-  ./configure $PKG_FLAGS
-  make
-  cd ..
-fi
-
-# Only install profiling package if we'll run coverage checks
-if [ ! -z "$RUNCOVERAGE" ]; then
-  echo "Downloading $PROFILING..."
-  curl -LO https://github.com/gap-packages/profiling/releases/download/v$PROFILING/profiling-$PROFILING.tar.gz
-  tar xzf profiling-$PROFILING.tar.gz
-  rm profiling-$PROFILING.tar.gz
-  mv profiling-$PROFILING profiling
-  cd profiling
-  ./autogen.sh
-  ./configure $PKG_FLAGS
-  make
-  cd ..
-fi
-
-# Only install gaplint if we'll run the linting checks
-if [ ! -z "$RUNGAPLINT" ]; then
-  echo "Downloading gaplint..."
+  sudo pip install cpplint
   git clone -b master --depth=1 https://github.com/james-d-mitchell/gaplint gaplint
-  cd ..
+  mkdir gap gap/.git gap/pkg
+  mv Semigroups gap/pkg/semigroups
+
+elif [ ! -z "$GAP_BRANCH" ]; then
+
+  echo -e "\nInstalling GAP..."
+  git clone -b $GAP_BRANCH --depth=1 https://github.com/gap-system/gap.git gap
+  cd gap
+  ./autogen.sh
+  ./configure --with-gmp=system $GAP_FLAGS
+  make
+  mkdir pkg
+  cd pkg
+  PKG_DIR=`pwd`
+  mv ../../Semigroups semigroups
+
+  echo -e "\nDownloading libsemigroups..."
+  cd semigroups/src
+  git clone https://github.com/james-d-mitchell/libsemigroups.git libsemigroups
+  cd libsemigroups
+  LIBSEMIGROUPS_VERS=`git tag | grep "v\d\+.\d\+.\d\+" | tail -n 1`
+  echo "Checking out libsemigroups version $LIBSEMIGROUPS_VERS..."
+  git checkout $LIBSEMIGROUPS_VERS
+  cd ../..
+
+  echo -e "\nCompiling the Semigroups package..."
+  ./autogen.sh
+  ./configure $PKG_FLAGS
+  make
+
+  echo -e "\nCloning the Digraphs package..."
+  cd $PKG_DIR
+  git clone https://github.com/gap-packages/Digraphs.git digraphs
+
+  if [ ! -z "$COVERAGE" ]; then
+    echo -e "\nDownloading the profiling package..."
+    git clone https://github.com/gap-packages/profiling.git profiling
+    cd profiling
+    PROFILING_VERS=`git tag | grep "v\d\+.\d\+.\d\+" | tail -n 1`
+    echo "Checking out profiling version $PROFILING_VERS..."
+    git checkout $PROFILING_VERS
+    ./autogen.sh
+    ./configure $PKG_FLAGS
+    make
+    cd ..
+  fi
+
+  INSTALL_PKG () {
+    echo -e "\nDownloading $1..."
+    cd $PKG_DIR
+    git clone https://github.com/gap-packages/$1.git $1
+    cd semigroups
+    VERSION=v`grep "\"$1\"" PackageInfo.g | awk -F'"' '{print $4}' | cut -c3-`
+    cd ../$1
+    echo "Checking out $1 $VERSION..."
+    git checkout $VERSION
+    if $2; then
+      ./autogen.sh
+      ./configure $PKG_FLAGS
+      make
+    fi
+    cd ..
+  }
+
+  INSTALL_PKG "genss" false
+  INSTALL_PKG "io"    true
+  INSTALL_PKG "orb"   true
+
+  echo -e "\nDownloading $GAPDOC..."
+  cd $PKG_DIR
+  curl -O https://www.gap-system.org/pub/gap/gap4/tar.gz/packages/$GAPDOC.tar.gz
+  tar xzf $GAPDOC.tar.gz
+  rm $GAPDOC.tar.gz
 fi
--- scripts/travis-test.sh
+++ scripts/travis-test.sh
@@ -2,51 +2,68 @@
 set -e
 set -o pipefail
 
-# Start in the GAP directory
-cd ../..
-GAPDIR=`pwd`
-
 # Create the testlog and remember its location
-touch testlog.txt
-TESTLOG="$GAPDIR/testlog.txt"
-
-cd pkg/$PACKAGE
-
-# Run gaplint
-if [ ! -z "$RUNGAPLINT" ]; then
-  echo -e "\nRunning gaplint..."
-  export PATH=$PATH:$GAPDIR/pkg/gaplint
-  make lint
-else
-  echo -e "\nNot running gaplint..."
-fi
-
-# Run the SaveWorkspace tests
-cd tst/workspaces
-echo -e "\nRunning SaveWorkspace tests..."
-echo "LoadPackage(\"$PACKAGE\", false); SemigroupsTestInstall(); Test(\"save-workspace.tst\"); quit; quit; quit;" | $GAPDIR/bin/gap.sh -A -r -m 1g -T 2>&1 | tee -a $TESTLOG
-echo "Test(\"load-workspace.tst\"); SemigroupsTestInstall(); quit; quit; quit;" | $GAPDIR/bin/gap.sh -L test-output.w -A -r -m 1g -T 2>&1 | tee -a $TESTLOG
-rm test-output.w
 cd ../..
+touch testlog.txt
+TESTLOG="`pwd`/testlog.txt"
 
-# Run all tests and manual examples
-echo -e "\nRunning tests and manual examples..."
-echo "LoadPackage(\"$PACKAGE\"); SemigroupsTestStandard(); SEMIGROUPS.TestManualExamples(); quit; quit; quit;" | $GAPDIR/bin/gap.sh -A -r -m 1g -T 2>&1 | tee $TESTLOG
+if [ ! -z "$LINT" ]; then
 
-# Run coverage checks
-if [ ! -z "$RUNCOVERAGE" ]; then
-  echo -e "\nPerforming code coverage tests..."
-  for TESTFILE in tst/standard/*.tst; do
-    FILENAME=${TESTFILE##*/}
-    if [ ! `grep -E "$FILENAME" .covignore` ]; then
-      ../digraphs/scripts/travis-coverage.py $TESTFILE $THRESHOLD | tee -a $TESTLOG
+  cd ../gaplint
+  export PATH=$PATH:`pwd`
+  cd ../gap/pkg/semigroups
+  # Can't use make lint since it requires compilation
+  `grep "^\s\+gaplint" Makefile.am`
+  `grep "^\s\+cpplint" Makefile.am`
+
+elif [ ! -z "$GAP_BRANCH" ]; then
+
+  GAP_DIR=`pwd`
+
+  # Work out the Digraphs versions/branches in which to run the tests
+  if [ ! -z "$DIGRAPHS_BR" ]; then
+    DIGRAPHS_BRANCHES=($DIGRAPHS_BR)
+  else
+    cd $GAP_DIR/pkg/semigroups
+    DIGRAPHS_VERS=v`grep "\"digraphs\"" PackageInfo.g| awk -F'"' '{print $4}' | cut -c3-`
+    DIGRAPHS_BRANCHES=("master" "$DIGRAPHS_VERS")
+  fi
+
+  for DIGRAPHS_BRANCH in "${DIGRAPHS_BRANCHES[@]}"; do
+    echo -e "\nRunning tests with Digraphs package in $DIGRAPHS_BRANCH..."
+    cd $GAP_DIR/pkg/digraphs
+    git checkout $DIGRAPHS_BRANCH
+    echo "Compiling Digraphs..."
+    ./autogen.sh
+    ./configure $PKG_FLAGS
+    make
+    cd $GAP_DIR/pkg/semigroups
+
+    if [ ! -z "$COVERAGE" ]; then
+
+      echo -e "\nPerforming code coverage tests..."
+      for TESTFILE in tst/standard/*.tst; do
+        FILENAME=${TESTFILE##*/}
+        if [ ! `grep -E "$FILENAME" .covignore` ]; then
+          ../digraphs/scripts/travis-coverage.py $TESTFILE $THRESHOLD | tee -a $TESTLOG
+        else
+          echo -e "\033[35mignoring $FILENAME, since it is listed in .covignore\033[0m"
+        fi
+      done
+
     else
-      echo -e "\033[35mignoring $FILENAME, since it is listed in .covignore\033[0m"
+      # Run the SaveWorkspace tests
+      cd tst/workspaces
+      echo -e "\nRunning SaveWorkspace tests..."
+      echo "LoadPackage(\"semigroups\", false); SemigroupsTestInstall(); Test(\"save-workspace.tst\"); quit; quit; quit;" | $GAP_DIR/bin/gap.sh -A -r -m 1g -T 2>&1 | tee -a $TESTLOG
+      echo "Test(\"load-workspace.tst\"); SemigroupsTestInstall(); quit; quit; quit;" | $GAP_DIR/bin/gap.sh -L test-output.w -A -r -m 1g -T 2>&1 | tee -a $TESTLOG
+      rm test-output.w
+      cd ../..
+      # Run all tests and manual examples
+      echo -e "\nRunning tests and manual examples..."
+      echo "LoadPackage(\"semigroups\"); SemigroupsTestStandard(); SEMIGROUPS.TestManualExamples(); quit; quit; quit;" | $GAP_DIR/bin/gap.sh -A -r -m 1g -T 2>&1 | tee $TESTLOG
     fi
   done
-else
-  echo -e "\nNot performing code coverage tests..."
 fi
 
-# Check the logs for invalid phrases
 ( ! grep -E "Diff|brk>|#E|Error|error|# WARNING|fail|Syntax warning|Couldn't open saved workspace|insufficient|WARNING in|FAILED|Total errors found:" $TESTLOG )
