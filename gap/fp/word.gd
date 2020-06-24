0a8987b50887c0f1c893f116f54351c2cc145faa
James D. Mitchell <jdm3@st-andrews.ac.uk>
2020-06-24 17:12:25 +0100

    MT's branch completely squashed

---
+++ gap/fp/word.gd
@@ -0,0 +1,12 @@
+#############################################################################
+##
+##  word.gd
+##  Copyright (C) 2020                                      Maria Tsalakou
+##
+##  Licensing information can be found in the README file of this package.
+##
+#############################################################################
+##
+
+DeclareOperation("WordToString", [IsString, IsList]);
+DeclareOperation("RandomWord", [IsInt, IsInt]);
