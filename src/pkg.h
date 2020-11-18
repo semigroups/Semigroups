c1f14751b6ca451b816f0a290fc2cf61d91b276f
James D. Mitchell <jdm3@st-andrews.ac.uk>
2020-11-18 20:08:48 +0000

    Remove some legacy code for GAP 4.9

--- src/pkg.h
+++ src/pkg.h
@@ -48,14 +48,6 @@
 #include "rnams.h"
 #include "semigroups-debug.h"
 
-
-#if !defined(GAP_KERNEL_MAJOR_VERSION) || GAP_KERNEL_MAJOR_VERSION < 3
-// compatibility with GAP <= 4.9
-static inline Obj NEW_PLIST_IMM(UInt type, Int plen) {
-  return NEW_PLIST(type | IMMUTABLE, plen);
-}
-#endif
-
 // The following typedefs are used in the Semigroups package kernel module code
 // to increase the readability of the code.
 
