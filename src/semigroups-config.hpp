6f68f3da3caa3406428917a0b76500d51a7481f4
James D. Mitchell <jdm3@st-andrews.ac.uk>
2021-03-16 17:55:12 +0000

    Update

---
+++ src/semigroups-config.hpp
@@ -0,0 +1,143 @@
+#ifndef _SRC_SEMIGROUPS_CONFIG_HPP
+#define _SRC_SEMIGROUPS_CONFIG_HPP 1
+ 
+/* src/semigroups-config.hpp. Generated automatically at end of configure. */
+/* src/_pkgconfig.h.  Generated from pkgconfig.h.in by configure.  */
+/* cnf/pkgconfig.h.in.  Generated from configure.ac by autoheader.  */
+
+/* define if the compiler supports basic C++14 syntax */
+#ifndef SEMIGROUPS_HAVE_CXX14
+#define SEMIGROUPS_HAVE_CXX14 1
+#endif
+
+/* Define to 1 if you have the <dlfcn.h> header file. */
+#ifndef SEMIGROUPS_HAVE_DLFCN_H
+#define SEMIGROUPS_HAVE_DLFCN_H 1
+#endif
+
+/* Define to 1 if you have the <inttypes.h> header file. */
+#ifndef SEMIGROUPS_HAVE_INTTYPES_H
+#define SEMIGROUPS_HAVE_INTTYPES_H 1
+#endif
+
+/* Define to 1 if you have the `pthread' library (-lpthread). */
+#ifndef SEMIGROUPS_HAVE_LIBPTHREAD
+#define SEMIGROUPS_HAVE_LIBPTHREAD 1
+#endif
+
+/* Define if you have POSIX threads libraries and header files. */
+#ifndef SEMIGROUPS_HAVE_PTHREAD
+#define SEMIGROUPS_HAVE_PTHREAD 1
+#endif
+
+/* Have PTHREAD_PRIO_INHERIT. */
+#ifndef SEMIGROUPS_HAVE_PTHREAD_PRIO_INHERIT
+#define SEMIGROUPS_HAVE_PTHREAD_PRIO_INHERIT 1
+#endif
+
+/* Define to 1 if you have the <stdint.h> header file. */
+#ifndef SEMIGROUPS_HAVE_STDINT_H
+#define SEMIGROUPS_HAVE_STDINT_H 1
+#endif
+
+/* Define to 1 if you have the <stdio.h> header file. */
+#ifndef SEMIGROUPS_HAVE_STDIO_H
+#define SEMIGROUPS_HAVE_STDIO_H 1
+#endif
+
+/* Define to 1 if you have the <stdlib.h> header file. */
+#ifndef SEMIGROUPS_HAVE_STDLIB_H
+#define SEMIGROUPS_HAVE_STDLIB_H 1
+#endif
+
+/* Define to 1 if you have the <strings.h> header file. */
+#ifndef SEMIGROUPS_HAVE_STRINGS_H
+#define SEMIGROUPS_HAVE_STRINGS_H 1
+#endif
+
+/* Define to 1 if you have the <string.h> header file. */
+#ifndef SEMIGROUPS_HAVE_STRING_H
+#define SEMIGROUPS_HAVE_STRING_H 1
+#endif
+
+/* Define to 1 if you have the <sys/stat.h> header file. */
+#ifndef SEMIGROUPS_HAVE_SYS_STAT_H
+#define SEMIGROUPS_HAVE_SYS_STAT_H 1
+#endif
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+#ifndef SEMIGROUPS_HAVE_SYS_TYPES_H
+#define SEMIGROUPS_HAVE_SYS_TYPES_H 1
+#endif
+
+/* Define to 1 if you have the <unistd.h> header file. */
+#ifndef SEMIGROUPS_HAVE_UNISTD_H
+#define SEMIGROUPS_HAVE_UNISTD_H 1
+#endif
+
+/* define if building in debug mode */
+/* #undef KERNEL_DEBUG */
+
+/* Define to the sub-directory where libtool stores uninstalled libraries. */
+#ifndef SEMIGROUPS_LT_OBJDIR
+#define SEMIGROUPS_LT_OBJDIR ".libs/"
+#endif
+
+/* Name of package */
+#ifndef SEMIGROUPS_PACKAGE
+#define SEMIGROUPS_PACKAGE "semigroups"
+#endif
+
+/* Define to the address where bug reports for this package should be sent. */
+#ifndef SEMIGROUPS_PACKAGE_BUGREPORT
+#define SEMIGROUPS_PACKAGE_BUGREPORT ""
+#endif
+
+/* Define to the full name of this package. */
+#ifndef SEMIGROUPS_PACKAGE_NAME
+#define SEMIGROUPS_PACKAGE_NAME "semigroups"
+#endif
+
+/* Define to the full name and version of this package. */
+#ifndef SEMIGROUPS_PACKAGE_STRING
+#define SEMIGROUPS_PACKAGE_STRING "semigroups 3.4.2"
+#endif
+
+/* Define to the one symbol short name of this package. */
+#ifndef SEMIGROUPS_PACKAGE_TARNAME
+#define SEMIGROUPS_PACKAGE_TARNAME "semigroups"
+#endif
+
+/* Define to the home page for this package. */
+#ifndef SEMIGROUPS_PACKAGE_URL
+#define SEMIGROUPS_PACKAGE_URL ""
+#endif
+
+/* Define to the version of this package. */
+#ifndef SEMIGROUPS_PACKAGE_VERSION
+#define SEMIGROUPS_PACKAGE_VERSION "3.4.2"
+#endif
+
+/* Define to necessary symbol if this constant uses a non-standard name on
+   your system. */
+/* #undef PTHREAD_CREATE_JOINABLE */
+
+/* Define to 1 if all of the C90 standard headers exist (not just the ones
+   required in a freestanding environment). This macro is provided for
+   backward compatibility; new code need not use it. */
+#ifndef SEMIGROUPS_STDC_HEADERS
+#define SEMIGROUPS_STDC_HEADERS 1
+#endif
+
+/* are we on CYGWIN? */
+#ifndef SEMIGROUPS_SYS_IS_CYGWIN32
+#define SEMIGROUPS_SYS_IS_CYGWIN32 0
+#endif
+
+/* Version number of package */
+#ifndef SEMIGROUPS_VERSION
+#define SEMIGROUPS_VERSION "3.4.2"
+#endif
+ 
+/* once: _SRC_SEMIGROUPS_CONFIG_HPP */
+#endif
