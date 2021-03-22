2f9e115248007989deb74b6a097bfd5fb9e83458
James D. Mitchell <jdm3@st-andrews.ac.uk>
2021-03-22 18:01:39 +0000

    Cleanup more

--- src/froidure-pin-fallback.cpp
+++ src/froidure-pin-fallback.cpp
@@ -16,7 +16,7 @@
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //
 
-#include "fropin.h"
+#include "froidure-pin-fallback.hpp"
 
 #include <string.h>  // for size_t, memcpy
 
