deadc026080f89d333723131cd6bfd7bc700ef41
JosephDayngerRuiz <jdruiz@arizona.edu>
2024-09-16 09:51:14 -0700

    Matching the other gd files
    Removed empty space.
    Added header.
    Change line endings to LF

--- gap/attributes/cartan.gd
+++ gap/attributes/cartan.gd
@@ -1,91 +1,40 @@
+#############################################################################
+##
+##  cartan.gd
+##  Copyright (C) 2024                                   Balthazar Charles
+##                                                             Joseph Ruiz
+##
+##  Licensing information can be found in the README file of this package.
+##
+#############################################################################
+##
 
 DeclareAttribute("TransversalIdempotents", IsSemigroup);
-
-
-
 DeclareCategory("IsGeneralisedConjugacyClass", IsObject);
-
 DeclareAttribute("Representative",IsGeneralisedConjugacyClass);
-
 DeclareAttribute("ParentAttr",IsGeneralisedConjugacyClass);
-
 DeclareOperation("GeneralisedConjugacyClass", [IsSemigroup, IsObject]);
-
-
-
-
-
 DeclareAttribute("GeneralisedConjugacyClassesRepresentatives", IsSemigroup);
-
 DeclareAttribute("GeneralisedConjugacyClasses", IsSemigroup);
-
-
-
-
-
 DeclareCategory("IsMonoidCharacterTable", IsObject);
-
 DeclareAttribute("ParentAttr",IsMonoidCharacterTable);
-
-
-
-
-
-
 DeclareAttribute("MonoidCharacterTable",IsSemigroup);
-
-
-
-
-
-
-
 DeclareCategory("IsMonoidCharacter", IsObject);
-
 DeclareOperation("MonoidCharacter",[IsMonoidCharacterTable, IsList]);
-
-DeclareOperation("PimMonoidCharacter",[IsMonoidCharacterTable, IsDenseList, IsMonoidCharacter]);
-
+DeclareOperation("PimMonoidCharacter",
+                 [IsMonoidCharacterTable, IsDenseList, IsMonoidCharacter]);
 DeclareAttribute("ParentAttr",IsMonoidCharacter);
-
 DeclareAttribute("ValuesOfMonoidClassFunction",IsMonoidCharacterTable);
-
 DeclareAttribute("ProjectiveCoverOf",IsMonoidCharacter);
-
 DeclareAttribute("ValuesOfCompositionFactorsFunction",IsMonoidCharacter);
-
-
-
-
-
-
-
 DeclareAttribute("DClassBicharacter", IsGreensDClass);
-
 DeclareAttribute("RegularRepresentationBicharacter", IsSemigroup);
-
 DeclareAttribute("RClassBicharacterOfGroupHClass", IsGroupHClass);
-
 DeclareAttribute("RClassRadicalOfGroupHClass", IsGroupHClass);
-
 DeclareAttribute("RClassRadicalBicharacterOfGroupHClass", IsGroupHClass);
-
 DeclareAttribute("DiagonalOfCharacterTables",IsSemigroup);
-
-
-
-
-
-
 DeclareAttribute("Irr",IsMonoidCharacterTable);
-
 DeclareAttribute("Pims",IsMonoidCharacterTable);
-
-
-
-
-
-
-# DeclareAttribute("MonoidCartanMatrix",IsSemigroup);
 DeclareOperation("MonoidCartanMatrix",[IsSemigroup]);
 
++
