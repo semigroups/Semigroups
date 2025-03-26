86ef32567475754b15654997798e1da4668f9c71
Pramoth Ragavan <107881923+pramothragavan@users.noreply.github.com>
2025-03-26 16:46:26 +0000

    PermuteMultiplicationTable, refactor OnMultiplicationTable, add docs

---
+++ src/isomorph.hpp
@@ -0,0 +1,16 @@
+#ifndef ISOMORPH_HPP
+#define ISOMORPH_HPP
+
+#include "gap_all.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+Obj PermuteMultiplicationTable(Obj self, Obj temp, Obj M, Obj p);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
