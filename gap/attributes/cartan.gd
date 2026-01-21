625ad599b0fb0f67d866e714c6c31cb57d85dc3e
JosephDayngerRuiz <114962122+JosephDayngerRuiz@users.noreply.github.com>
2026-01-21 13:02:51 -0700

    Added all planned declarations

--- gap/attributes/cartan.gd
+++ gap/attributes/cartan.gd
@@ -16,10 +16,20 @@ DeclareOperation("GeneralizedConjugacyClass",
                  [IsSemigroup, IsMultiplicativeElement]);
 DeclareAttribute("GeneralizedConjugacyClassesRepresentatives", IsSemigroup);
 DeclareAttribute("GeneralizedConjugacyClasses", IsSemigroup);
+
 DeclareCategory("IsMonoidCharacterTable", IsObject);
 DeclareAttribute("ParentAttr", IsMonoidCharacterTable);
 DeclareAttribute("Irr", IsMonoidCharacterTable);
+DeclareAttribute("ComputedMonoidBrauerTables", IsMonoidCharacterTable);  # todo
 DeclareAttribute("MonoidCharacterTable", IsSemigroup);
+
+# todo block
+DeclareCategory("IsMonoidBrauerTable", IsObject);
+DeclareAttribute("MonoidCharacterTable", IsMonoidBrauerTable);
+DeclareAttribute("Irr", IsMonoidBrauerTable);
+DeclareAttribute("UnderlyingCharacteristic", IsMonoidBrauerTable);
+DeclareAttribute("UnderlyingField", IsMonoidBrauerTable);
+# todo end block
 
 DeclareCategory("IsMonoidCharacter", IsObject);
 DeclareOperation("MonoidCharacter", [IsMonoidCharacterTable, IsList]);
@@ -29,6 +39,19 @@ DeclareAttribute("ParentAttr", IsMonoidCharacter);
 DeclareAttribute("ValuesOfMonoidClassFunction", IsMonoidCharacter);
 DeclareAttribute("ProjectiveCoverOf", IsMonoidCharacter);
 DeclareAttribute("ValuesOfCompositionFactorsFunction", IsMonoidCharacter);
+
+# todo block
+DeclareCategory("IsMonoidModularCharacter", IsObject);
+DeclareOperation("MonoidModularCharacter", [IsMonoidBrauerTable, IsList]);
+DeclareOperation("PimMonoidModularCharacter",
+                 [IsMonoidBrauerTable, IsDenseList, IsMonoidModularCharacter]);
+DeclareAttribute("ParentAttr", IsMonoidModularCharacter);
+DeclareAttribute("ValuesOfMonoidPClassFunction", IsMonoidCharacter);
+DeclareAttribute("ProjectiveCoverOf", IsMonoidModularCharacter);
+DeclareAttribute("ValuesOfCompositionFactorsFunction",
+                 IsMonoidModularCharacter);
+# todo end block
+
 DeclareAttribute("DClassBicharacter", IsGreensDClass);
 DeclareAttribute("RegularRepresentationBicharacter", IsSemigroup);
 DeclareAttribute("RClassBicharacterOfGroupHClass", IsGroupHClass);
